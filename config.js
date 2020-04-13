1f27e9e7ae6bd888e124f5d3ffed8f9ca76c86b8
Willy Nzesseu <willynzesseu@gmail.com>
2020-04-13 16:31:26 +0100

    build: set port 80

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   ENV: process.env.NODE_ENV || 'development',
-  PORT: process.env.PORT || 3000,
+  PORT: process.env.PORT || 80,
   URL: process.env.BASE_URL || 'http://localhost:3000',
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://abc123:oLW4JS5Eo9VNpy7T@cluster0-a5t6d.mongodb.net/test?authSource=admin&retryWrites=true&w=majority',
   JWT_SECRET: process.env.JWT_SECRET || 'secret1'
